7097063e433eb3f25091a73b7f367bd4487e7ef5
chaninkou <michaelkoulove@yahoo.com>
2019-09-12 01:56:07 -0700

    Great problem. Find powerset of unique sets with duplicate input

---
+++ src/subsetsII/FindAllUnqiueSubsetsWithDuplicateInputFunction.java
@@ -0,0 +1,48 @@
+package subsetsII;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllUnqiueSubsetsWithDuplicateInputFunction {
+	// Contain duplicate subsets
+	// Need to be sorted, unless it does not include duplicate
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+		// Contain total of all subset
+		List<List<Integer>> powerSet = new ArrayList<>();
+
+		// Sort the array first
+		Arrays.sort(nums);
+
+		// Backtracking with 0 as starting point, pass in empty arraylist
+		backtrack(powerSet, new ArrayList<>(), nums, 0);
+
+		return powerSet;
+	}
+
+	private void backtrack(List<List<Integer>> powerSet, List<Integer> tempList, int[] nums, int start) {
+		// We are using new ArrayList cause we don't want it to reference to it
+		powerSet.add(new ArrayList<>(tempList));
+
+		// We try to get all the set from start and then move on to the next
+		// element
+		for (int i = start; i < nums.length; i++) {
+			// Skip duplicates
+			// If i > start, that means it is going to have the same length as
+			// the previous set and then we check if they are the same
+			if (i > start && nums[i] == nums[i - 1]) {
+				continue;
+			}
+			// Update the tempList
+			tempList.add(nums[i]);
+
+			// So we go down the list until we reach i = nums.length which will
+			// stop the loop
+			backtrack(powerSet, tempList, nums, i + 1);
+
+			// Remove the last element of the tempList
+			tempList.remove(tempList.size() - 1);
+		}
+
+	}
+}
---
+++ src/subsetsII/Main.java
@@ -0,0 +1,15 @@
+package subsetsII;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] nums = {1,2,2,3};
+		
+		System.out.println("Input: " + Arrays.toString(nums)); 
+		
+		FindAllUnqiueSubsetsWithDuplicateInputFunction solution = new FindAllUnqiueSubsetsWithDuplicateInputFunction();
+		
+		System.out.println("Powerset without duplicate set: " +solution.subsetsWithDup(nums));
+	}
+}
